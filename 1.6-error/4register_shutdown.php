2d5f219f47cb8d10677b5404777ae2fea2d70ee1
jiangyong <1107985549@qq.com>
2018-08-06 10:50:47 +0800

    checkin

--- 1.6-error/4register_shutdown.php
+++ 1.6-error/4register_shutdown.php
@@ -1,4 +1,16 @@
 <?php
 class Shutdown
-{__halt_compiler()
+{
+	public function stop()
+	{
+		if (error_get_last()) {
+			print_r(error_get_last());
+		}
+		die('Stop.');
+	}
 }
+
+register_shutdown_function(array(new Shutdown(), 'stop'));
+
+$a = new a();
+echo '必须终止';
